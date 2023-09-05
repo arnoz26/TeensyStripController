fc68eb8aa9116471d9127f21b2554cc637ef7abf
Arnoz <arnoz@arnoz.com>
2023-09-05 17:56:10 +0200

    Update
    - Code clean
    - Chaser in Pink
    - "X" command addes to launch chaser test

--- TeensyStripController.ino
+++ TeensyStripController.ino
@@ -13,8 +13,8 @@
  ** https://github.com/PaulStoffregen/OctoWS2811
  ** http://www.pjrc.com/teensy/td_libs_OctoWS2811.html 
  ** 
- *********************************************************************************************************/  
- /*  
+ *********************************************************************************************************/
+/*  
     License:
     --------   
     Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -49,7 +49,7 @@
      
 */
 
-#include "OctoWS2811Ext.h" //A slightly hacked version of the OctoWS2811 lib which allows for dynamic setting of the number of leds is used.
+#include "OctoWS2811Ext.h"  //A slightly hacked version of the OctoWS2811 lib which allows for dynamic setting of the number of leds is used.
 
 //Definiton of Major and Minor part of the firmware version. This value can be received using the V command.
 //If something is changed in the code the number should be increased.
@@ -59,11 +59,11 @@
 //For Teensy 4.0 you can define the nuber of ouput Pins - 8 should be good (for 3.1/3.2 this is only used for calculation and schould not changed)
 const int numPins = 8;
 //for tesnsy 4.0 you can change the standard port (schould not be done) - Not used for 3.1/3.2
-byte pinList[numPins] = {2, 14, 7, 8, 6, 20, 21, 5};
+byte pinList[numPins] = { 2, 14, 7, 8, 6, 20, 21, 5 };
 
 //Defines the max number of leds which is allowed per ledstrip.
 //This number is fine for Teensy 3.2, 3.1. For newer Teensy versions (they dont exists yet) it might be possible to increase this number.
-#define MaxLedsPerStrip 2100
+#define MaxLedsPerStrip 1200
 
 //Defines the Pinnumber to which the built in led of the Teensy is connected.
 //For Teensy 3.2, 3.1 this is pin 13, if you use a newer Teensy version (not available at the time of writing) you might need to change this number.
@@ -89,19 +89,19 @@ int BlinkMode;
 elapsedMillis BlinkModeTimeoutTimer;
 
 //Config definition for the OctoWS2811 lib
-const int config = WS2811_RGB | WS2811_800kHz; //Dont change the color order (even if your strip are GRB). DOF takes care of this issue (see config of ledstrip toy)
+const int config = WS2811_RGB | WS2811_800kHz;  //Dont change the color order (even if your strip are GRB). DOF takes care of this issue (see config of ledstrip toy)
 
 #if defined(__IMXRT1062__)
-  OctoWS2811 leds(MaxLedsPerStrip, displayMemory, drawingMemory, config, numPins, pinList);
+OctoWS2811 leds(MaxLedsPerStrip, displayMemory, drawingMemory, config, numPins, pinList);
 #else
-  OctoWS2811 leds(MaxLedsPerStrip, displayMemory, drawingMemory, config);
+OctoWS2811 leds(MaxLedsPerStrip, displayMemory, drawingMemory, config);
 #endif
 
-word configuredStripLength=MaxLedsPerStrip-100;
+word configuredStripLength = MaxLedsPerStrip - 100;
 
 //Setup of the system. Is called once on startup.
 void setup() {
-  Serial.begin(9600); // This has no effect. USB bitrate (12MB) will be used anyway.
+  Serial.begin(9600);  // This has no effect. USB bitrate (12MB) will be used anyway.
 
   //Initialize the lib for the leds
   leds.setStripLength(configuredStripLength);
@@ -109,12 +109,12 @@ void setup() {
   leds.show();
 
   //Initialize the led pin
-  pinMode(LedPin,OUTPUT);
+  pinMode(LedPin, OUTPUT);
 
   //Initialize and find value of the test pin
-  pinMode(TestPin,INPUT_PULLUP);  
-  digitalWrite(TestPin,1);
-  
+  pinMode(TestPin, INPUT_PULLUP);
+  digitalWrite(TestPin, 1);
+
   SetBlinkMode(0);
 }
 
@@ -124,8 +124,8 @@ void loop() {
 
   //Check if data is available
   if (Serial.available()) {
-    
-    byte receivedByte = Serial.read();  
+
+    byte receivedByte = Serial.read();
     switch (receivedByte) {
       case 'L':
         //Set length of strips
@@ -134,7 +134,7 @@ void loop() {
       case 'F':
         //Fill strip area with color
         Fill();
-        break;  
+        break;
       case 'R':
         //receive data for strips
         ReceiveData();
@@ -142,161 +142,161 @@ void loop() {
       case 'O':
         //output data on strip
         OutputData();
-        break;   
+        break;
       case 'C':
         //Clears all previously received led data
         ClearAllLedData();
         break;
       case 'V':
         //Send the firmware version
-        SendVersion();  
+        SendVersion();
         break;
       case 'M':
-        //Get max number of leds per strip  
+        //Get max number of leds per strip
         SendMaxNumberOfLeds();
         break;
       case 'T':
-        //initiate Test over serial  
+        //initiate Test over serial
         Ack();
         Test();
+        break;
+      case 'X':
+        //initiate Test over serial
+        Ack();
+        Chaser();
         break;
       default:
         // no unknown commands allowed. Send NACK (N)
         Nack();
-      break;
+        break;
     }
-   
+
     SetBlinkMode(1);
-        
-  } 
+  }
   Blink();
 
   // TEST ROUTINES BUTTON DETECTION
 
-	if (digitalRead(TestPin) == LOW) {
-	//Button pressed
-		if (buttonActive == false) {
-			buttonActive = true;
-			buttonTimer = millis();
-		}
-		if ((millis() - buttonTimer > longPressTime) && (longPressActive == false)) {
-			longPressActive = true;
-        Chaser();
-		}
-	} else {
-		if (buttonActive == true) {
-			if (longPressActive == true) {
-				longPressActive = false;
-			} else {
+  if (digitalRead(TestPin) == LOW) {
+    //Button pressed
+    if (buttonActive == false) {
+      buttonActive = true;
+      buttonTimer = millis();
+    }
+    if ((millis() - buttonTimer > longPressTime) && (longPressActive == false)) {
+      longPressActive = true;
+      Chaser();
+    }
+  } else {
+    if (buttonActive == true) {
+      if (longPressActive == true) {
+        longPressActive = false;
+      } else {
         Test();
-			}
-			buttonActive = false;
-		}
-	}
-
+      }
+      buttonActive = false;
+    }
+  }
 }
 
 //Sets the mode for the blinking of the led
 void SetBlinkMode(int Mode) {
-  BlinkMode=Mode;
-  BlinkModeTimeoutTimer=0;
+  BlinkMode = Mode;
+  BlinkModeTimeoutTimer = 0;
 }
 
 //Controls the blinking of the led
 void Blink() {
-  switch(BlinkMode) {
+  switch (BlinkMode) {
     case 0:
       //Blinkmode 0 is only active after the start of the Teensy until the first command is received.
-      if(BlinkTimer<1500) {
-        digitalWrite(LedPin,0);
-      } else if(BlinkTimer<1600) {
-        digitalWrite(LedPin,1);
+      if (BlinkTimer < 1500) {
+        digitalWrite(LedPin, 0);
+      } else if (BlinkTimer < 1600) {
+        digitalWrite(LedPin, 1);
       } else {
-        BlinkTimer=0;
-        digitalWrite(LedPin,0);
+        BlinkTimer = 0;
+        digitalWrite(LedPin, 0);
       }
-    break;
+      break;
     case 1:
       //Blinkmode 1 is activated when the Teensy receives a command
       //Mode expires 500ms after the last command has been received resp. mode has been set
-      if(BlinkTimer>30) {
-        BlinkTimer=0;
-        digitalWrite(LedPin,!digitalRead(LedPin));
+      if (BlinkTimer > 30) {
+        BlinkTimer = 0;
+        digitalWrite(LedPin, !digitalRead(LedPin));
       }
-      if(BlinkModeTimeoutTimer>500) {
+      if (BlinkModeTimeoutTimer > 500) {
         SetBlinkMode(2);
       }
-    break;   
+      break;
     case 2:
       //Blinkmode 2 is active while the Teensy is waiting for more commands
-      if(BlinkTimer<1500) {
-        digitalWrite(LedPin,0);
-      } else if(BlinkTimer<1600) {
-        digitalWrite(LedPin,1);
-      } else if(BlinkTimer<1700) {
-        digitalWrite(LedPin,0);
-      } else if(BlinkTimer<1800) {
-        digitalWrite(LedPin,1);
-      }else {
-        BlinkTimer=0;
-        digitalWrite(LedPin,0);
+      if (BlinkTimer < 1500) {
+        digitalWrite(LedPin, 0);
+      } else if (BlinkTimer < 1600) {
+        digitalWrite(LedPin, 1);
+      } else if (BlinkTimer < 1700) {
+        digitalWrite(LedPin, 0);
+      } else if (BlinkTimer < 1800) {
+        digitalWrite(LedPin, 1);
+      } else {
+        BlinkTimer = 0;
+        digitalWrite(LedPin, 0);
       }
-      default:
+    default:
       //This should never be active
       //The code is only here to make it easier to determine if a wrong Blinkcode has been set
-      if(BlinkTimer>2000) {
-        BlinkTimer=0;
-        digitalWrite(LedPin,!digitalRead(LedPin));
+      if (BlinkTimer > 2000) {
+        BlinkTimer = 0;
+        digitalWrite(LedPin, !digitalRead(LedPin));
       }
       break;
   }
-  
 }
 
 
 //Outputs the data in the ram to the ledstrips
 void OutputData() {
-   leds.show();
-   Ack();
+  leds.show();
+  Ack();
 }
 
 
 //Fills the given area of a ledstrip with a color
 void Fill() {
-  word firstLed=ReceiveWord();
+  word firstLed = ReceiveWord();
 
-  word numberOfLeds=ReceiveWord();
+  word numberOfLeds = ReceiveWord();
 
-  int ColorData=ReceiveColorData();
+  int ColorData = ReceiveColorData();
 
-   if( firstLed<=configuredStripLength*8 && numberOfLeds>0 && firstLed+numberOfLeds-1<=configuredStripLength*8 ) {
-       word endLedNr=firstLed+numberOfLeds;
-       for(word ledNr=firstLed; ledNr<endLedNr;ledNr++) {
-         leds.setPixel(ledNr,ColorData);
-       }
-       Ack();
-   } else {
-     //Number of the first led or the number of leds to receive is outside the allowed range
-     Nack();    
-   }
-  
-  
+  if (firstLed <= configuredStripLength * 8 && numberOfLeds > 0 && firstLed + numberOfLeds - 1 <= configuredStripLength * 8) {
+    word endLedNr = firstLed + numberOfLeds;
+    for (word ledNr = firstLed; ledNr < endLedNr; ledNr++) {
+      leds.setPixel(ledNr, ColorData);
+    }
+    Ack();
+  } else {
+    //Number of the first led or the number of leds to receive is outside the allowed range
+    Nack();
+  }
 }
 
 
 //Receives data for the ledstrips
 void ReceiveData() {
-  word firstLed=ReceiveWord();
+  word firstLed = ReceiveWord();
 
-  word numberOfLeds=ReceiveWord();
+  word numberOfLeds = ReceiveWord();
 
-  if( firstLed<=configuredStripLength*8 && numberOfLeds>0 && firstLed+numberOfLeds-1<=configuredStripLength*8 ) {
-    //FirstLedNr and numberOfLeds are valid. 
+  if (firstLed <= configuredStripLength * 8 && numberOfLeds > 0 && firstLed + numberOfLeds - 1 <= configuredStripLength * 8) {
+    //FirstLedNr and numberOfLeds are valid.
     //Receive and set color data
-    
-    word endLedNr=firstLed+numberOfLeds;
-    for(word ledNr=firstLed; ledNr<endLedNr;ledNr++) {
-      leds.setPixel(ledNr,ReceiveColorData());
+
+    word endLedNr = firstLed + numberOfLeds;
+    for (word ledNr = firstLed; ledNr < endLedNr; ledNr++) {
+      leds.setPixel(ledNr, ReceiveColorData());
     }
 
     Ack();
@@ -309,24 +309,24 @@ void ReceiveData() {
 
 //Sets the length of the longest connected ledstrip. Length is restricted to the max number of allowed leds
 void SetLedStripLength() {
-   word stripLength=ReceiveWord();
-   if(stripLength<1 || stripLength>MaxLedsPerStrip) {
-     //stripLength is either to small or above the max number of leds allowed
-     Nack();
-   } else {
-     //stripLength is in the valid range
-     configuredStripLength=stripLength;
-     leds.setStripLength(stripLength);
-     leds.begin();  //Reinitialize the OctoWS2811 lib (not sure if this is needed)
+  word stripLength = ReceiveWord();
+  if (stripLength < 1 || stripLength > MaxLedsPerStrip) {
+    //stripLength is either to small or above the max number of leds allowed
+    Nack();
+  } else {
+    //stripLength is in the valid range
+    configuredStripLength = stripLength;
+    leds.setStripLength(stripLength);
+    leds.begin();  //Reinitialize the OctoWS2811 lib (not sure if this is needed)
 
-     Ack();
-   }
+    Ack();
+  }
 }
 
 //Clears the data for all configured leds
-void  ClearAllLedData() {
-  for(word ledNr=0;ledNr<configuredStripLength*8;ledNr++) {
-    leds.setPixel(ledNr,0);
+void ClearAllLedData() {
+  for (word ledNr = 0; ledNr < configuredStripLength * 8; ledNr++) {
+    leds.setPixel(ledNr, 0);
   }
   Ack();
 }
@@ -341,9 +341,9 @@ void SendVersion() {
 
 //Sends the max number of leds per strip
 void SendMaxNumberOfLeds() {
-  byte B=MaxLedsPerStrip>>8;
+  byte B = MaxLedsPerStrip >> 8;
   Serial.write(B);
-  B=MaxLedsPerStrip&255;
+  B = MaxLedsPerStrip & 255;
   Serial.write(B);
   Ack();
 }
@@ -361,25 +361,23 @@ void Nack() {
 
 //Receives 3 bytes of color data.
 int ReceiveColorData() {
-  while(!Serial.available()) {};
-  int colorValue=Serial.read(); 
-  while(!Serial.available()) {};
-  colorValue=(colorValue<<8)|Serial.read();
-  while(!Serial.available()) {};
-  colorValue=(colorValue<<8)|Serial.read();
-  
+  while (!Serial.available()) {};
+  int colorValue = Serial.read();
+  while (!Serial.available()) {};
+  colorValue = (colorValue << 8) | Serial.read();
+  while (!Serial.available()) {};
+  colorValue = (colorValue << 8) | Serial.read();
+
   return colorValue;
-
-
 }
 
 //Receives a word value. High byte first, low byte second
 word ReceiveWord() {
-  while(!Serial.available()) {};
-  word wordValue=Serial.read()<<8; 
-  while(!Serial.available()) {};
-  wordValue=wordValue|Serial.read();
-  
+  while (!Serial.available()) {};
+  word wordValue = Serial.read() << 8;
+  while (!Serial.available()) {};
+  wordValue = wordValue | Serial.read();
+
   return wordValue;
 }
 
@@ -396,14 +394,14 @@ word ReceiveWord() {
 */
 
 // Less intense colors for testing - assumes WS2812 color order of G, R, B
-#define RED    0x001600
-#define GREEN  0x160000
-#define BLUE   0x000016
+#define RED 0x001600
+#define GREEN 0x160000
+#define BLUE 0x000016
 #define YELLOW 0x141000
-#define PINK   0x001209
+#define PINK 0x001209
 #define ORANGE 0x041000
-#define WHITE  0x101010
-#define BLACK  0x000000
+#define WHITE 0x101010
+#define BLACK 0x000000
 
 
 void Test() {
@@ -419,31 +417,28 @@ void Test() {
   ColorWipe(BLACK, 10);
 }
 
-void ColorWipe(int color, int wait)
-{   
-  for (int i=0; i < leds.numPixels(); i++) {
+void ColorWipe(int color, int wait) {
+  for (int i = 0; i < leds.numPixels(); i++) {
     leds.setPixel(i, color);
   }
 
   // wait before turning on LEDs and also turn on indicator LED
   delayMicroseconds(100000);
-  digitalWrite(LedPin,1);
+  digitalWrite(LedPin, 1);
   leds.show();
 
   // wait for desginated timeout and then turn off indicator LED
   delayMicroseconds(wait);
-  digitalWrite(LedPin,0);
-}  
+  digitalWrite(LedPin, 0);
+}
 
-void Chaser()
-{   
-  for (int i=0; i < leds.numPixels(); i++) {
-    leds.setPixel(i, WHITE);
+void Chaser() {
+  for (int i = 0; i < leds.numPixels(); i++) {
+    leds.setPixel(i, PINK);
     leds.show();
     delay(50);
-
   }
   delay(2000);
   ColorWipe(BLACK, 10);
   leds.show();
-}  
+}
